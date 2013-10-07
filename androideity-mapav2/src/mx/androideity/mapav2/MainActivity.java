162b32764369ab66cae44456e50c3b3c1eed4421
hector <torrhes@gmail.com>
2013-10-07 12:34:04 -0500

    first commit

---
+++ androideity-mapav2/src/mx/androideity/mapav2/MainActivity.java
@@ -0,0 +1,20 @@
+package mx.androideity.mapav2;
+
+import android.os.Bundle;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }*/
+    
+}
